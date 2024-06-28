9ab327b4a7d5201ec79739fb6b186951e506421b
Matheus de Barros <162741395+barrosmatheusc@users.noreply.github.com>
2024-06-28 09:34:40 -0300

    update

---
+++ cadastro/cadastro.js
@@ -0,0 +1,27 @@
+function showsenha() {
+    const senhaInput = document.getElementById('senha');
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+    } else {
+        senhaInput.type = 'password';
+    }
+}
+
+
+function confirmacao() {
+    const senhaInput = document.getElementById('confirmar-senha');
+    if (senhaInput.type === 'password') {
+        senhaInput.type = 'text';
+    } else {
+        senhaInput.type = 'password';
+    }
+}
+
+function formatartelefone(input) {
+    let telefone = input.value.replace(/\D/g, ''); // Usar input.value em vez de numero
+    
+    // Corrigir a expressão regular para formatar o telefone corretamente
+    telefone = telefone.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+    
+    input.value = telefone;
+}
--- login/login.js
+++ login/login.js
@@ -11,14 +11,23 @@ function checkbox() {
 
 
 function login() {
-    var login = document.getElementById('Login').value;
-    var senha = document.getElementById('senha').value;
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('password').value;
 
     if(login == "82998380275" && senha == "matheusb1234") {
         alert('Login efetuado com sucesso!');
+        location.href = "../home/index.html";
     }
     else {
         alert('Login ou Senha errado, insira novamente e efetue o login')
     }
 }
 
+function formatartelefone(input) {
+    let telefone = input.value.replace(/\D/g, ''); // Usar input.value em vez de numero
+    
+    // Corrigir a expressão regular para formatar o telefone corretamente
+    telefone = telefone.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3');
+    
+    input.value = telefone;
+}
